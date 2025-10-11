a7f504ff0ac4ace1b4d5b8c3456b2ee524976c0a
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-10-11 13:10:16 +0200

    1. Codigos comentados para no olvidar que hace que 2. Cuando el jugador toque el calamar, se comvierte en su hijo para que se mueva a la vez 3. Cuando el jugador deja de tocar el calamar, deja de ser su hijo

--- Colorincolorcolorinescolorado/Assets/Scripts/Calamar/Calamar.cs
+++ Colorincolorcolorinescolorado/Assets/Scripts/Calamar/Calamar.cs
@@ -3,61 +3,70 @@ using System.Collections;
 
 public class Calamar : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float TimeOnSquid;
-    bool isOnSquid = false;
-    [SerializeField] bool dead = false;
-    public Mesh mallaVivo;
-    public Mesh mallaMuerto;
-    Mesh malla;
-    public Canvas canvas;
+    [SerializeField] float speed; //variable de velocidad
+    [SerializeField] float TimeOnSquid; //tiempo que pasa desde que el jugador sube al calamar, hasta que este muere
+    bool isOnSquid = false; //booleano para saber si el jugador está sobre el calamar (e iniciar la corutina)
+    [SerializeField] bool dead = false; //booleano para saber si el calamar está muerto o vivo
+    public Mesh mallaVivo; //malla del calamar vivo
+    public Mesh mallaMuerto; //mall del calamar muerto
+    Mesh malla; //variabkle malla para programar mas comodo
+    public Canvas canvas; //variable del canvas
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-        malla = mallaVivo;
-        this.GetComponent<MeshFilter>().mesh = mallaVivo;
-        canvas.enabled = false;
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>(); //encuentra el canvas en la escena y lo asigna a la variable
+        malla = mallaVivo; //asigna la mallaVivo a la variable malla
+        this.GetComponent<MeshFilter>().mesh = mallaVivo; //asigna la mallaVivo al MeshFilter (lo que hace que el calamar se vea de una forma u otra) del calamar
+        canvas.enabled = false; //se asegura de que el canvas no se muestre en pantalla al inicio
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<MeshFilter>().mesh = malla;
+        this.GetComponent<MeshFilter>().mesh = malla; //asigna la malla (vivo o muerto) al MeshFilter del calamar
         transform.Translate(0, 0, speed * Time.deltaTime); //se mueve en z, invertir speed para que vaya a -z
-        if (isOnSquid)
+        if (isOnSquid) //si el jugador está sobre el calamar
         {
-            StartCoroutine(time());
+            StartCoroutine(time()); //llama a la corrutina
         }
 
-        if (dead)
+        if (dead) //si el calamar está muerto
         {
-           malla = mallaMuerto;
-        }
-    }
-
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            isOnSquid = true;
-            transform.parent = other.transform;
+           malla = mallaMuerto; //cambia la malla a la de muerto
         }
     }
 
-    void OnCollisionStay(Collision other)
+
+    void OnCollisionEnter(Collision other) //cuando hay una colision
     {
-        if (other.gameObject.CompareTag("Player") && dead == true)
+        if (other.gameObject.CompareTag("Player")) //si el otro objeto tiene el tag player
         {
-           canvas.enabled = true;
+            isOnSquid = true; //cambia el booleano a true
+            other.transform.SetParent(this.transform); //hace que el jugador sea hijo del calamar
         }
     }
 
-    IEnumerator time()
+    void OnCollisionStay(Collision other) //mientras haya una colision
     {
-        yield return new WaitForSeconds(TimeOnSquid);
-        dead = true;
+        if (other.gameObject.CompareTag("Player") && dead == true) //si el otro objeto tiene el tag player y el calamar está muerto
+        {
+            //el personaje muere
+           canvas.enabled = true; //muestra el canvas en pantalla
+        }
+    }
+
+    void OnCollisionExit(Collision other) //cuando la colision termina
+    {
+        if (other.gameObject.CompareTag("Player")) //si el otro objeto tiene el tag player
+        {
+            other.transform.SetParent(null); //hace que el jugador deje de ser hijo del calamar
+        }
+    }
+
+    IEnumerator time() //corutina que espera un tiempo y luego mata al calamar
+    {
+        yield return new WaitForSeconds(TimeOnSquid); //espera el tiempo asignado en TimeOnSquid
+        dead = true; //cambia el booleano a true (el calamar está muerto)
     }
 }
--- Colorincolorcolorinescolorado/Assets/Scripts/Tiburón/Tiburón.cs
+++ Colorincolorcolorinescolorado/Assets/Scripts/Tiburón/Tiburón.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class Tiburón : MonoBehaviour
 {
     public Canvas canvas;
-    [SerializeField] float speed;
+    [SerializeField] float speed; //variable de velocidad
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-        canvas.enabled = false;
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>(); //Busca el canvas en la escena, y lo asigna a l avariable
+        canvas.enabled = false; //se asegura de que el canvas no se muestre en pantalla al inicio
     }
 
     // Update is called once per frame
@@ -22,8 +22,7 @@ public class Tiburón : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             //acaba la partida (cuando se haga el menú)
-            Debug.Log("Game Over");
-            canvas.enabled = true;
+            canvas.enabled = true; //muestrta el canvas en pantalla
         }
     }
 }
