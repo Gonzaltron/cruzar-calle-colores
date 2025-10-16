2b6f90c9741ffcc26cc731bba420ece260c28617
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-10-16 13:42:57 +0200

    Camara funcional (preguntar como hacer para suavizar

--- Colorincolorcolorinescolorado/Assets/Scripts/Cammera/Cammera.cs
+++ Colorincolorcolorinescolorado/Assets/Scripts/Cammera/Cammera.cs
@@ -1,16 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Cammera : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+public class Cammera : MonoBehaviour {
+
+
+    public GameObject player; // drag player gameobject into this in inspector
+    float Speed;
+    public float SpeedNormal;
+    public float SpeedFast;
+    [SerializeField] Canvas canvas;
+    public bool isSmoothFollow = true;
+    float distanceZstart;
+    void Start ()
     {
-        
+        canvas = FindObjectOfType<Canvas>();
+        canvas.enabled = false;
+        distanceZstart = player.transform.position.z - transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceZ = player.transform.position.z - transform.position.z;
+        Vector3 newCameraPosition = transform.position + new Vector3(0, 0, Speed * Time.deltaTime);
+        transform.position = newCameraPosition;
+
+
+
+        if (distanceZ > 6)
+        {
+            Speed = SpeedFast;
+        }
+        else
+        {
+            Speed = SpeedNormal;
+        }
+
+        if (distanceZ < 2)
+        {
+            canvas.enabled = true;
+            Speed = 0;
+            //animacion de muerte
+            //quitarle el control al jugador
+            //...
+        }
     }
 }
