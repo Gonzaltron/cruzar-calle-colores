898aa8d9a3604eaeb68ae18df8041b5b8fd0dee6
Naiara <naiara_igartua@euneiz.com>
2025-10-31 17:51:35 +0100

    scripts comentados

---
+++ Colorincolorcolorinescolorado/Assets/Scripts/Player/Cambiocolor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class cambio : MonoBehaviour
+{
+    private Renderer rendererObject;
+
+    public Color defaultColor;
+
+    public Color newColor;
+
+    public bool colorChanged = false;   
+    void Start()
+    {
+        rendererObject = GetComponent<Renderer>();  
+
+        rendererObject.material.color = defaultColor;   //Se inicializa el color al color principal
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))        //Seleccionar la tecla E para cambiar el color
+        {
+            if (!colorChanged)      
+            {
+                ChangeColorMethod(newColor);        //Cambiar el color inicial al segundo color si el inicial está puesto
+
+            }
+            else
+            {
+                ChangeColorMethod(defaultColor);    //Si el color inicial no está seleccionado entonces significa que está el secundario puesto por lo tanto se cambia al inicial 
+            }
+        }
+    }
+
+    public void ChangeColorMethod(Color colorToChange)
+    {
+        colorChanged = !colorChanged;
+        rendererObject.material.color = colorToChange;
+    }
+}
+
