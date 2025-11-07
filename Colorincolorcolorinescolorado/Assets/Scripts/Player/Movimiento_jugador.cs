263c70b5a0469d7b8007c37e7ec6b3a6e32a4d1c
Oliver <oliver_herrero@euneiz.com>
2025-11-07 15:51:25 +0100

    ya va el overlapbox

--- Colorincolorcolorinescolorado/Assets/Scripts/Player/Movimiento_jugador.cs
+++ Colorincolorcolorinescolorado/Assets/Scripts/Player/Movimiento_jugador.cs
@@ -26,12 +26,12 @@ public class Movimiento_jugador: MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            direction = new Vector2(0, 1);
+            direction = new Vector2(1, 0);
             Movement();
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            direction = new Vector2(1, 0);
+            direction = new Vector2(0, 1);
             Movement();
         }
         else if (Input.GetKeyDown(KeyCode.S))
@@ -63,12 +63,19 @@ public class Movimiento_jugador: MonoBehaviour
 
     void Movement()
     {
-
+        
         Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position + new Vector3(direction.x, -1, direction.y), Vector3.one*0.2f, Quaternion.identity);
         int i = 0;
 
         while (i < hitColliders.Length)
         {
+            Casilla casilla = hitColliders[i].GetComponent<Casilla>();
+            if (casilla != null)
+            {
+                Vector3 p = casilla.getPosition();
+                transform.position = p;
+                break;
+            }
             i++;
         }
     }
