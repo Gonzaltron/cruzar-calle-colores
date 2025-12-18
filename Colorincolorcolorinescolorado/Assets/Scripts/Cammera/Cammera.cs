76589b8f24914e807e8317a13cf2cba41e7292af
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2025-12-18 09:48:31 +0100

    Arreglado camara paso 1

--- Colorincolorcolorinescolorado/Assets/Scripts/Cammera/Cammera.cs
+++ Colorincolorcolorinescolorado/Assets/Scripts/Cammera/Cammera.cs
@@ -15,13 +15,11 @@ public class Cammera : MonoBehaviour
     public float smoothSpeed;               //suavizado de cambio de velocidad
     public float sidewaisMovementDuration;  //duraci�n del movimiento lateral
     public float sideMovement;              //distancia del movimiento lateral
-    private float distanceZ;
+    public float distanceZ;
     public bool activo;
     bool unaVez;
     void Start()
     {
-        canvas = FindObjectOfType<Canvas>();                                   //busca el canvas en la escena
-        canvas.enabled = false;                                                //desactiva el canvas al inicio
         distanceZstart = player.transform.position.z - transform.position.z;   //calcula la distancia inicial entre c�mara y jugador
         unaVez = false;
     }
@@ -36,26 +34,29 @@ public class Cammera : MonoBehaviour
             ReduceSpeed();          //Cuando el jugador est� cerca, reduce la velocidad
             StopCamera();           //Cuando el jugador est� muy cerca, muestra el canvas y detiene la c�mara
             CameraMovement();       //Movimiento lateral de la c�mara con las flechas izquierda y derecha
-        } 
+        }
+        else
+        {
+            Speed = 0;
+        }
         if(!unaVez)
         {
             CameraPositionStart();  //Actualiza la posicion de la camara
             CameraSpeed();          //Cambia la velocidad de la camara seg�n el jugador                       
             ReduceSpeed();          //Cuando el jugador est� cerca, reduce la velocidad
             StopCamera();           //Cuando el jugador est� muy cerca, muestra el canvas y detiene la c�mara
-            CameraMovement(); 
+            CameraMovement();       //Movimiento lateral de la c�mara con las flechas izquierda y derecha
             unaVez = true; 
         }  
     }
     private void CameraPositionStart()
     {
-        float distanceZ = player.transform.position.z - transform.position.z;                       //calcula la distancia entre c�mara y jugador
-        Vector3 newCameraPosition = transform.position + new Vector3(0, 0, Speed * Time.deltaTime); //calcula la nueva posici�n de la c�mara
-        transform.position = newCameraPosition;                                                     //actualiza la posicion de la camara
+        distanceZ = player.transform.position.z - transform.position.z;                       //calcula la distancia entre c�mara y jugador
+        transform.DOMoveZ(transform.position.z + Speed*Time.deltaTime, 0.1f); //mueve la camara
     }
     private void CameraSpeed()        //cambia la velocidad de la c�mara seg�n la distancia al jugador
     {
-        if (distanceZ < 6)
+        if (distanceZ < 30)
         {
             DOTween.To(() => Speed, x => Speed = x, SpeedNormal, smoothSpeed);  //cambia la velocidad a SpeedFast con suavizado
         }
@@ -66,14 +67,14 @@ public class Cammera : MonoBehaviour
     }
     private void ReduceSpeed()        //cuando el jugador est� cerca, reduce la velocidad   
     {
-        if (distanceZ < 6 && distanceZ > 2)
+        if (distanceZ < 30 && distanceZ > 10)
         {
             DOTween.To(() => Speed, x => Speed = x, SpeedNormal, smoothSpeed); //cambia la velocidad a SpeedNormal con suavizado
         }
     }
     private void StopCamera()       //cuando el jugador esaa muy cerca, muestra el canvas y detiene la c�mara
     {
-        if (distanceZ < 2)
+        if (distanceZ < 10)
         {
             canvas.enabled = true;
             Speed = 0;
@@ -84,11 +85,11 @@ public class Cammera : MonoBehaviour
     }
     private void CameraMovement()       //movimiento lateral de la c�mara con las flechas izquierda y derecha
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))                                              //si se pulsa la flecha izquierda
+        if (Input.GetKeyDown(KeyCode.A))                                              //si se pulsa la flecha izquierda
         {
             transform.DOMoveX(transform.position.x - sideMovement, sidewaisMovementDuration); //mueve la c�mara a la izquierda, con duraci�n sidewaisMovementDuration
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))                                             //si se pulsa la flecha derecha
+        if (Input.GetKeyDown(KeyCode.D))                                             //si se pulsa la flecha derecha
         {
             transform.DOMoveX(transform.position.x + sideMovement, sidewaisMovementDuration); //mueve la c�mara a la derecha, con duraci�n sidewaisMovementDuration
         }
